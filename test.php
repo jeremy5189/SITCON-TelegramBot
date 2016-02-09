08ce633a6a4df48c083c6c9835ccf135c361c74d
Jeremy Yen <jeremy5189@gmail.com>
2016-02-10 00:50:20 +0800

    Add test

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$regex = '/([^\\.]|^)(s[Ii][Tt][Cc][Oo][Nn]|[sS]i[Tt][Cc][Oo][Nn]|[sS][Ii]t[Cc][Oo][Nn]|[sS][Ii][Tt]c[Oo][Nn]|[sS][Ii][Tt][Cc]o[Nn]|[sS][Ii][Tt][Cc][Oo]n)([^a-zA-Z.]|$)/';
+
+$case = array(
+	'SITCON' => 0,
+	'sitcon' => 1,
+	'sitCOn' => 1,
+	'sitcon SITCON' => 1,
+	'SITCON sitcon' => 1,
+	'fjeaioewSITCONfajewi' => 0,
+	'fjiawejfiawejsiconfajewiofjeiow' => 0
+);
+
+foreach( $case as $message => $expect ) {
+	echo $message . " : " . ( preg_match($regex, $message) == $expect ? 'pass' : 'fail' ) . "\n";
+}
+	
+	
